e9f051a5a7d517f4e34dcf4cc252ca1c738a2bdd
egorbakan <118420911+egorbakan@users.noreply.github.com>
2022-11-22 13:59:44 +0900

    Add files via upload

---
+++ lib2/NewLib/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace NewLib
+{
+    public class Class1
+    {
+        public void Priceecho(string name)
+        {
+            Console.WriteLine($"Привет, стоимость продуктов будет = {name}");
+        }
+
+        public void Heightecho(string name)
+        {
+            Console.WriteLine($" = {name}");
+        }
+
+        public int Price_sum(int price)
+        {
+            return price + 1520;
+        }
+        public float Price_sum2(int price)
+        {
+            return price + 265;
+        }
+        public int Price_sum3(int heigth)
+        {
+            return heigth + 15;
+        }
+    }
+}
---
+++ lib2/lib2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NewLib;
+
+namespace lib2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 func = new Class1();
+
+            Console.WriteLine($"Стоимость первого товара будет =  {func.Price_sum(20)}");
+            Console.WriteLine($"Стоимость второго товара будет =  {func.Price_sum2(20)}");
+            Console.WriteLine($"Привет, рост через 5 лет будет =  {func.Price_sum3(150)}");
+
+        }
+    }
+}
